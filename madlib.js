7b71b02276f110cc448ed301550c8f07de6589ae
Troy Denney <tvdenney@gmail.com>
2016-10-10 13:46:20 -0400

    completed madlib

---
+++ madlib.js
@@ -0,0 +1,80 @@
+var noun1 = 'woman'
+var verb1 = 'jostle'
+var noun2 = 'baby lion'
+var adjective1 = 'ferocious'
+var noun3 = 'manager'
+var verb2 = 'studied'
+var verb3 = 'beheading'
+var verb4 = 'initiate'
+var adjective2 = 'gross'
+var adjective3 = 'magnificent'
+var noun4 = 'family'
+var noun5 = 'dollar bill'
+var verb5 = 'drove'
+var verb6 = 'put'
+var verb7 = 'catch'
+var verb8 = 'eat'
+var noun6 = 'money'
+var noun7 = 'mother'
+var adjective4 = 'quiet'
+var adjective5 = 'large'
+
+var sentence = `The ${noun1} of a big company needed to call one of his employees
+about an urgent problem with one of the main computers.  He ${verb1}
+the employee's home phone number and was greeted with a child's
+whisper, Hello?
+
+Feeling a bit put out at the inconvenience of having to talk to a
+${noun2}, the boss asked, Is your daddy home?
+
+Yes, whispered the ${adjective1} voice.
+
+May I talk with him? the ${noun3} asked.
+
+To the ${verb2} of the boss, the small voice whispered, No.
+
+Wanting to ${verb3} with an adult, the boss asked, Is your mommy
+there?
+
+Yes, came the answer.
+
+May I ${verb4} with her?
+
+Again the ${adjective2} voice whispered, No.
+
+Knowing that it was not likely that a ${adjective3} ${noun4} would be left
+home alone, the ${noun5} decided he would just ${verb5} a message with
+the person who should be there ${verb6} over the child.
+
+Is there any one there besides you? the boss asked the child.
+
+Yes, ${verb7} the child, a policeman.
+
+${verb8} what a cop would be doing at his employee's home, the
+boss asked, May I speak with the policeman?
+
+No, he is busy, whispered the child.
+
+Busy doing what? asked the boss.
+
+Talking to ${noun6} and ${noun7} and the fireman, came the whispered
+ answer.
+
+Growing concerned and even worried as he heard what sounded like a
+helicopter through the ear piece on the phone the boss asked,
+What is that noise?
+
+A hello-copper, answered the whispering voice.
+
+What is going on there? asked the boss, now alarmed.
+
+In an ${adjective4} whispering voice the child answered, The search team
+just landed the hello-copper!
+
+Alarmed, concerned and more than just a ${adjective5}  frustrated the boss
+asked, The search team? Why are they there?
+
+Still whispering, the young voice replied along with a muffled
+giggle, They are looking for me!`
+
+console.log(sentence);
